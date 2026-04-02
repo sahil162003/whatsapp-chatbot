931005ea21840b77a0d9579af68e0d2682519f8e
Sahil <sahilchalke3692@gmail.com>
2026-04-02 23:09:22 +0530

    Fix component scan

--- src/main/java/com/sc/WhatsappChatbotApplication.java
+++ src/main/java/com/sc/WhatsappChatbotApplication.java
@@ -3,7 +3,8 @@ package com.sc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.sc")
+
 public class WhatsappChatbotApplication {
 
     public static void main(String[] args) {
